2c12a53ceebb8d6e81c5da2b503cd27db4dafc2c
Vishal Kumar <amvishal09@outlook.com>
2023-01-13 19:46:37 +0530

    added window event

--- script.js
+++ script.js
@@ -37,32 +37,8 @@ for (let i = 2; i > 0; i--) {
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-// The setInterval function updates the current time every second and checks if the current time is equal to the alarm time.
-setInterval(() => {
-    let date = new Date(),
-        h = date.getHours(),
-        m = date.getMinutes(),
-        s = date.getSeconds(),
-        ampm = "AM";
-    if (h >= 12) {
-        h = h - 12;
-        ampm = "PM";
-    }
-    h = h == 0 ? h = 12 : h;
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
-
-    if (alarmTime === `${h}:${m} ${ampm}`) {
-        ringtone.play();
-        ringtone.loop = true;
-    }
-});
-
 // The setAlarm function is used to set and clear
-
-function setAlarm() {
+window.addEventListener("click", () => {
     if (isAlarmSet) {
         alarmTime = "";
         ringtone.pause();
@@ -81,6 +57,26 @@ function setAlarm() {
     upcomingAlarm.innerHTML = time
     content.classList.add("disable");
     setAlarmBtn.innerText = "Clear Alarm";
-}
+})
 
-setAlarmBtn.addEventListener("click", setAlarm);
+// The setInterval function updates the current time every second and checks if the current time is equal to the alarm time.
+setInterval(() => {
+    let date = new Date(),
+        h = date.getHours(),
+        m = date.getMinutes(),
+        s = date.getSeconds(),
+        ampm = "AM";
+    if (h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+    if (alarmTime === `${h}:${m} ${ampm}`) {
+        ringtone.play();
+        ringtone.loop = true;
+    }
+});
